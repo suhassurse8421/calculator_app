a4c62b45870db6bf471cb6a32f1c7da0318137fb
suhas <suhas@plainsurf.com>
2023-05-04 12:25:37 +0530

    calculator app completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  const operations = ["+", "-", "*", "/"]
+
+  const [firstNumber, setFirstNumber] = useState("");
+
+  const [secondNumber, setSecondNumber] = useState("");
+
+  const [currentOperation, setCurrentOperation] = useState("");
+
+  const [result, setResult] = useState(0);
+
+  useEffect(() => {
+    console.log(result);
+  }, [result])
+
+
+  const clickNumbers = (val) => {
+    if (currentOperation === "") {
+      setFirstNumber(firstNumber + val);
+    } else {
+      setSecondNumber(secondNumber + val);
+    }
+  }
+
+  const clickOperations = (val) => {
+    setCurrentOperation(val);
+  }
+
+  const performOperation = () => {
+    switch (currentOperation) {
+      case "+":
+        setResult(Number(firstNumber) + Number(secondNumber));
+        break;
+      case "-":
+        setResult(Number(firstNumber) - Number(secondNumber));
+        break;
+      case "*":
+        setResult(Number(firstNumber) * Number(secondNumber));
+        break;
+      case "/":
+        setResult(Number(firstNumber) / Number(secondNumber));
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calculator'>
+        <div className='display'>
+          {firstNumber + "" + currentOperation + secondNumber}
+          <br />
+          {"="}
+          <br />
+          {result === 0 ? null : result}
+        </div>
+        <div className='buttons'>
+          <div className='leftSide'>
+            <div id="result" onClick={() => { performOperation() }}>Result</div>
+            <div className='numbers'>
+              {numbers.map((val) => {
+                return <div id="individualNumber" onClick={() => { clickNumbers(val) }}>{val}</div>
+              })}
+            </div>
+          </div>
+          <div className='leftSide'>
+            {operations.map((val) => {
+              return <div id="operation" onClick={() => { clickOperations(val) }}>{val}</div>
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
